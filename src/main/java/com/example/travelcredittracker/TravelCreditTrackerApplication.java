c56760d3d381e9ba68112e32e91ad05a23705182
Alexandra Haderer <ahaderer@gmail.com>
2018-02-09 21:30:42 -0600

    Initial commit, Gradle

---
+++ src/main/java/com/example/travelcredittracker/TravelCreditTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.example.travelcredittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelCreditTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelCreditTrackerApplication.class, args);
+	}
+}
