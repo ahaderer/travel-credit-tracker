71d0d842e7a1293e62291e34bc69e4b942aa1ddf
Alexandra Haderer <ahaderer@gmail.com>
2018-02-11 01:46:01 -0600

    Added fields/constructors/methods to Credit class. Basic functionality to CreditController - view credits and "add credit" form.

--- src/main/java/com/example/travelcredittracker/controllers/CreditController.java
+++ src/main/java/com/example/travelcredittracker/controllers/CreditController.java
@@ -3,23 +3,42 @@ package com.example.travelcredittracker.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.ArrayList;
 
 @Controller
 @RequestMapping("credits")
 public class CreditController {
 
+    static ArrayList<String> credits = new ArrayList<>();
+
     @RequestMapping(value = "")
     public String index(Model model) {
-
-        ArrayList<String> credits = new ArrayList<>();
-        credits.add("first");
-        credits.add("second");
-        credits.add("third");
 
         model.addAttribute("credits", credits);
         model.addAttribute("title", "My Credits");
 
         return "credits/index";
     }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCreditForm(Model model) {
+
+        // TODO: Create a new branch to complete build on "add credit form"
+
+        model.addAttribute("title", "Add A Travel Credit");
+        return "credits/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCreditForm(@RequestParam String credit) {
+        credits.add(credit);
+
+        // Redirect to /credits
+        return "redirect:";
+    }
+
+
 }
--- src/main/java/com/example/travelcredittracker/models/Credit.java
+++ src/main/java/com/example/travelcredittracker/models/Credit.java
@@ -1,6 +1,85 @@
 package com.example.travelcredittracker.models;
 
+import java.util.Date;
+
 public class Credit {
 
+    private int id;
 
+    private String traveler;
+
+    private String airline;
+
+    private String confirmationNumber;
+
+    private int ticketNumber;
+
+    private Date expirationDate;
+
+    private int originalAmount;
+
+    public Credit() {
+    }
+
+    // TODO: regular constructor??
+    public Credit(String traveler, String airline, String confirmationNumber, int ticketNumber, Date expirationDate, int originalAmount) {
+        this.traveler = traveler;
+        this.airline = airline;
+        this.confirmationNumber = confirmationNumber;
+        this.ticketNumber = ticketNumber;
+        this.expirationDate = expirationDate;
+        this.originalAmount = originalAmount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTraveler() {
+        return traveler;
+    }
+
+    public void setTraveler(String traveler) {
+        this.traveler = traveler;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public String getConfirmationNumber() {
+        return confirmationNumber;
+    }
+
+    public void setConfirmationNumber(String confirmationNumber) {
+        this.confirmationNumber = confirmationNumber;
+    }
+
+    public int getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(int ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public int getOriginalAmount() {
+        return originalAmount;
+    }
+
+    public void setOriginalAmount(int originalAmount) {
+        this.originalAmount = originalAmount;
+    }
 }
